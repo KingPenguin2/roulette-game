3c0bdadeaff617c113cf0eed84547c4a17947dd5
Penguin <noreply>
2023-03-07 16:53:53 +0000

    added other sections to bet on and display in the output

--- main.cpp
+++ main.cpp
@@ -38,6 +38,7 @@ class Table{
         }
         int spin_wheel(){
             //Spins the "wheel and returns number between 0 and 36"
+            srand(time(NULL));
             return rand()%37 ;
         }
         int calculate_winnings(int code, int number, int amountBet){
@@ -58,16 +59,63 @@ class Table{
                 if (binary_search(blacks.begin(),blacks.end(), number)) {return amountBet * payouts[0];}
                 else { return 0;}
             }
+            else if (code == 41 && number <= 18 && number > 0){
+                return amountBet * payouts[2];
+            }
+            else if (code == 42 && number <= 36 && number > 18){
+                return amountBet * payouts[2];
+            }
+            else if (code == 43 && number <= 12 && number > 0){
+                return amountBet * payouts[1];
+            }
+            else if (code == 44 && number <= 24 && number > 12){
+                return amountBet * payouts[1];
+            }
+            else if (code == 45 && number <= 36 && number > 24){
+                return amountBet * payouts[1];
+            }
+            else if (code == 46){
+                if (binary_search(column1.begin(),column1.end(), number)) {return amountBet * payouts[1];}
+                else { return 0;}
+            }
+            else if (code == 47){
+                if (binary_search(column2.begin(),column2.end(), number)) {return amountBet * payouts[1];}
+                else { return 0;}
+            }
+            else if (code == 48){
+                if (binary_search(column3.begin(),column3.end(), number)) {return amountBet * payouts[1];}
+                else { return 0;}
+            }
             else{
                 // Loss
                 return 0;
             }
         }
 
+        void winning_message(int number){
+            string eveness = (number%2 == 0) ? "Even " : "Odd ";
+            string colour = (number == 0) ? "Green " : (binary_search(blacks.begin(),blacks.end(), number)) ? "Black " : "Red ";
+            string half = (number == 0) ? "None " : (number <= 18) ? "First " : "Last ";
+            string section;
+            if (number <= 12 && number > 0) {section = "s1 ";}
+            else if (number <= 24 && number > 12) { section = "s2 ";}
+            else if (number <= 36 && number > 24) { section = "s3 ";}
+
+            string column;
+            if (binary_search(column1.begin(),column1.end(), number)) {column = "c1 ";}
+            else if (binary_search(column2.begin(),column2.end(), number)) {column = "c2 ";}
+            else if (binary_search(column3.begin(),column3.end(), number)) {column = "c3 ";}
+            cout << "\t\t" << eveness << colour << half << section << column << endl << endl;
+            return;
+        }
+
     private:
         vector<int> payouts {1,2,35};
         vector<int> reds {1,3,5,7,9,12,14,16,18,19,21,23,25,27,30,32,34,36};
         vector<int> blacks {2,4,6,8,10,11,13,15,17,20,22,24,26,28,29,31,33,35};
+        vector<int> column1 {1,4,7,10,13,16,19,22,25,28,31,34};
+        vector<int> column2 {2,5,8,11,14,17,20,23,26,29,32,35};
+        vector<int> column3 {3,6,9,12,15,18,21,24,27,30,33,36};
 };
 
 int convertBetType(string betType){
@@ -87,6 +135,37 @@ int convertBetType(string betType){
     else if (betType == "red" || betType == "r")
     {
         code = 40;
+    }
+    else if (betType == "first" || betType == "f")
+    {
+        code = 41;
+    }
+    else if (betType == "last" || betType == "l")
+    {
+        code = 42;
+    }
+    else if (betType == "s1" )
+    {
+        code = 43;
+    }
+    else if (betType == "s2" )
+    {
+        code = 44;
+    }
+    else if (betType == "s3" )
+    {
+        code = 45;
+    }
+    else if (betType == "c1" )
+    {
+        code = 46;
+    }else if (betType == "c2")
+    {
+        code = 47;
+    }
+    else if (betType == "c3")
+    {
+        code = 48;
     }
     else{
         code = stoi(betType);
@@ -114,7 +193,7 @@ void play(){
             cin >> bet;
         }
 
-        cout << "what do you want to bet on (number / odd / even / black / red)" << endl;
+        cout << "what do you want to bet on (number / odd / even / black / red / first / last / s1 / s2 / s3 / c1 / c2/ c3)" << endl;
         cin >> betType;
         betType_code = convertBetType(betType);
         player.placeBet(bet);
@@ -126,7 +205,12 @@ void play(){
             cout.flush();
             this_thread::sleep_for(chrono::seconds(1));
         }
-        cout << number << "\n\n" << endl; 
+        
+
+        cout << number;
+        table.winning_message(number); 
+
+
         winnings = table.calculate_winnings(betType_code, number, bet);
         if (winnings> 0){
             // won money
